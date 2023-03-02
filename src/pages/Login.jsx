f61b194ea068d8975f856e49960f1bdd48c2377e
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-03-02 11:44:53 -0300

    pagina de login criada

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,38 @@
+import { useState } from 'react'
+
 import style from '../styles/Login.module.css'
 
 export default function Login() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        
+    }
+
     return (
-        <h1>login</h1>
+        <form onSubmit={handleSubmit} className={style.login_form}>
+            <h2>Login</h2>
+            <label>
+                <span>Email:</span>
+                <input 
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+            </label>
+
+            <label>
+                <span>Senha:</span>
+                <input 
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
+            </label>
+            <button className='btn'>Entrar</button>
+        </form>
     )
 }
