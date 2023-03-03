85439975c85724ca5c71e8e32cdc1fd8e05a0cc3
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-03-03 11:49:54 -0300

    formulario de cadastro criado

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,48 @@
+import { useState } from 'react'
+
 import style from '../styles/Signup.module.css'
 
 export default function Signup() {
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        
+    }
+
     return (
-        <h1>signup</h1>
+        <form onSubmit={handleSubmit} className={style.signup_form}>
+            <h2>Se cadastre</h2>
+            
+            <label>
+                <span>Nome de usuário:</span>
+                <input 
+                    type="text"
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username} 
+                />
+            </label>
+
+            <label>
+                <span>Email:</span>
+                <input 
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+            </label>
+
+            <label>
+                <span>Senha:</span>
+                <input 
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
+            </label>
+            <button className='btn'>Cadastrar</button>
+        </form>
     )
 }
