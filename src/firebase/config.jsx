fae6c9e228cb8f48c517ec98c12667391f1d623a
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-03-03 12:29:44 -0300

    firebase instalado e configurado

---
+++ src/firebase/config.jsx
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDk08lXUKhUk6NppeezAahqFKJxlL45IcM",
+    authDomain: "myfinance-541a7.firebaseapp.com",
+    projectId: "myfinance-541a7",
+    storageBucket: "myfinance-541a7.appspot.com",
+    messagingSenderId: "737583986695",
+    appId: "1:737583986695:web:84bb7021c88fb28f1a92a2"
+}
+
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
