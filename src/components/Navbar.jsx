28fd409ee32fcc067d11dd1c05e817da5346064a
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-03-06 10:58:13 -0300

    botao de logout criado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import { Link } from 'react-router-dom'
+import useLogout from '../hooks/useLogout'
+
 import style from '../styles/Navbar.module.css'
 
 export default function Navbar() {
+    const { logout } = useLogout()
+
     return (
         <nav className={style.navbar}>
             <ul>
@@ -9,6 +13,9 @@ export default function Navbar() {
                 <li>
                     <Link to='/login'>Login</Link>
                     <Link to='/signup'>Cadastro</Link>
+                </li>
+                <li>
+                    <button className='btn' onClick={logout}>Sair</button>
                 </li>
             </ul>
         </nav>
