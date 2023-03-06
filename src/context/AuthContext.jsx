c471a46bd3a9d4f7980cc123c362b2a985eb1446
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-03-06 10:36:22 -0300

    hook useLogout criado

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -5,6 +5,7 @@ export const AuthContext = createContext()
 export function authReducer(state, action) {
     switch(action.type) {
         case 'LOGIN': return { ...state, user: action.payload }
+        case 'LOGOUT': return { ...state, user: null }
         
         default: return state
     }
