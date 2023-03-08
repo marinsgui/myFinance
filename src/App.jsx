540c45b71f12867b893ae226fd1a65f4073bab6e
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-03-08 11:44:50 -0300

    route guarding

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, redirect } from 'react-router-dom'
+
+import useAuthContext from './hooks/useAuthContext'
 
 // components
 import Navbar from './components/Navbar'
@@ -10,15 +12,21 @@ import Signup from './pages/Signup'
 
 function App() {
 
+  const { authIsReady } = useAuthContext()
+
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
+          </Routes>
+        </BrowserRouter>
+      )}
+    </>
   )
 }
 
