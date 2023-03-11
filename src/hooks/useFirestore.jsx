c435020623063ef4a36be858379eea061f5005f8
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-03-11 11:23:11 -0300

    adicionadas timestamps nos novos documentos

--- src/hooks/useFirestore.jsx
+++ src/hooks/useFirestore.jsx
@@ -1,5 +1,5 @@
 import { useReducer, useEffect, useState } from 'react'
-import { projectFirestore } from '../firebase/config'
+import { projectFirestore, timestamp } from '../firebase/config'
 
 let initialState = {
     document: null,
@@ -34,7 +34,8 @@ export default function useFirestore(collection) {
         dispatch({ type: 'IS_PENDING' })
 
         try {
-            const addedDocument = await ref.add(doc)
+            const createdAt = timestamp.fromDate(new Date())
+            const addedDocument = await ref.add({ ...doc, createdAt })
             dispatchIfNotCancelled({ type: 'ADDED_DOCUMENT', payload: addedDocument })
         } catch(err) {
             dispatchIfNotCancelled({ type: 'ERROR', payload: err.message })
